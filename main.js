81ad4167b2609a5ddd4a9c5e11db8d64b7188d5f
Kolawole O. Gabriel <segaz2002@gmail.com>
2017-11-26 19:12:34 +0200

    removed comment

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 
 var coinApp = angular.module('coinApp', []);
 
-// Define the `PhoneListController` controller on the `phonecatApp` module
 coinApp.controller('mainController', function mainController($scope, $http) {
   $scope.title = "Bitcoin Exchange";
   $scope.rates = [];
 
   $scope.getPrice = function(){
     console.log("Trying to get price");   
-    // Simple GET request example:
     $http({
       method: 'GET',
       url: 'https://api.coindesk.com/v1/bpi/currentprice/CNY.json '
