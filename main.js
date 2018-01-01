ccf730b8394c19aed075b0ed9e7f4841f68a17a8
AJIBOYE OLASUNKANMI JOHN <33224920+OlaJohn-Ajiboye@users.noreply.github.com>
2018-01-01 22:43:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ coinApp.controller('mainController', function($scope, $http) {
     $scope.total_currency_amount = 0.0;
     $scope.total_usd_units = 0.0;
     $scope.total_currency_units = 0.0;
-
+   
+    $scope.currency_to_convert_to =  {code: 'NGN',  name: 'Nigerian Naira'};
 
     $scope.currenciesWithNames = [
       {code: 'EUR',  name: 'Euro'},
@@ -19,33 +20,19 @@ coinApp.controller('mainController', function($scope, $http) {
       {code: 'NGN',  name: 'Nigerian Naira'},
       {code: 'CNY',  name: 'Chinese Yuan'},
       {code: 'CAD',  name: 'Canadian Dollars'}
-      
-      
-
   ];
 
-
-  $scope.selectedCurrency = {code: 'EUR', symbol: '€'};
-  $scope.selectedCurrencyCode = 'USD';
-  $scope.selectedCurrency2 = {code: 'EUR', symbol: '€'};
-  $scope.selectedCurrency3 = {code: 'GBP', symbol: '£'};
-  $scope.selectedCurrency7 = {code: 'EUR', symbol: '€'};
-  $scope.selectedCurrency8 = {code: 'USD', symbol: '$'};
-  $scope.selectedCurrency9 = {code: 'GBP', symbol: '£'};
-  $scope.selectedCurrency10 = {code: 'GBP', symbol: '£'};
-  
-    $scope.currency_to_convert_to = 'EUR';
   
     $scope.getPrice = function() {
       console.log("Trying to get price");   
       $http({
         method: 'GET',
-        url: 'https://api.coindesk.com/v1/bpi/currentprice/' + $scope.currency_to_convert_to + '.json'
+        url: 'https://api.coindesk.com/v1/bpi/currentprice/' + $scope.currency_to_convert_to.code + '.json'
       }).then(function successCallback(response) {
         $scope.rates = [];
           var bpi = response.data.bpi
           $scope.rates.push(bpi['USD']);
-          $scope.rates.push(bpi[$scope.currency_to_convert_to]);
+          $scope.rates.push(bpi[$scope.currency_to_convert_to.code]);
         }, function errorCallback(response) {
           console.error(response);
         });
@@ -64,7 +51,7 @@ coinApp.controller('mainController', function($scope, $http) {
   
     $scope.calculateUnits = function() {
       angular.forEach($scope.rates, function(bt_rate) {
-        if (bt_rate.code == $scope.currency) {
+        if (bt_rate.code == $scope.currency.code) {
           var units = parseInt($scope.amount) / bt_rate.rate_float;
           if (bt_rate.code == "USD") {
             $scope.total_usd_units = units;
